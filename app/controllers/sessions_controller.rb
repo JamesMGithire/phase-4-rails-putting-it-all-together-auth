41306b370fa105f9cfac7f76c11a88e23de204f2
James Mburu Githire <jamesmburug24@gmail.com>
2022-11-27 19:08:20 +0300

    completed assignment

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+#   before_action :authorize
+
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: ["Invalid username or passsword"]}, status: :unauthorized
+        end
+    end
+
+    def destroy
+        return render json: {errors: ["Not authorized"]}, status: :unauthorized unless session.include? :user_id
+        session.delete :user_id
+        render json: {},status: :no_content
+    end
+    
+    # private
+    # def authorize
+    # end
+end
